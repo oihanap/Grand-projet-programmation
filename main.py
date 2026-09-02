cca8f9c82dc6e0a6e0b10801e6d8001ac05d53d6
oihanap <oihana.perisse@ipsa.fr>
2026-09-02 17:25:07 +0200

    project setup

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import sys
+from PySide6.QtWidgets import QApplication, QWidget, QMessageBox
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = QApplication(sys.argv)
 
+loader = QUiLoader()
+f = QFile("main.ui")
+f.open(QFile.ReadOnly)
+w = loader.load(f)
+f.close()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+w.btnHello.clicked.connect(lambda: QMessageBox.information(w, "Hello", "Hello World"))
+w.show()
+sys.exit(app.exec())
